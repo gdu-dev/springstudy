480cbfc90c57aa534d47e5672ddbb4fa3471f630
gdu-dev <minkt78@naver.com>
2023-04-11 10:18:02 +0900

    2023-04-11

--- 01_IoC_Container/src/main/java/com/gdu/app01/xml04/MyDAO.java
+++ 01_IoC_Container/src/main/java/com/gdu/app01/xml04/MyDAO.java
@@ -10,21 +10,36 @@ public class MyDAO {
 	// field
 	private Connection con;
 	
-	// singleton pattern
-	private static MyDAO dao = new MyDAO();
-	private MyDAO() { }
-	public static MyDAO getInstance() {
-		return dao;
-	}
+	// singleton pattern - app-context.xml에서 <bean> 태그를 만들 때 사용된다.
 	
 	// method
-	public void list() {
+	public Connection getConnection() {
 		
 		// Spring Container에 만들어 둔 myConn Bean 가져오기
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext("xml04/app-context.xml");
 		con = ctx.getBean("myConn", MyConnection.class).getConnection();
-		
 		ctx.close();
+		return con;
+		
+	}
+	
+	public void close() {
+		
+		try {
+			if(con != null) {
+				con.close();
+			} 
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void list() {
+		
+		con = getConnection();
+		
+		close();
 		
 	}
 	
