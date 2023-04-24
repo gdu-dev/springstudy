e75924b65f4019ee12f6402f257e8c748dbc9dac
gdu-dev <minkt78@naver.com>
2023-04-24 16:23:16 +0900

    2023-04-24

--- 08_Mybatis_Batch/src/main/java/com/gdu/app08/config/BatchConfig.java
+++ 08_Mybatis_Batch/src/main/java/com/gdu/app08/config/BatchConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.gdu.app08.batch.BoardCountAlertScheduler;
+import com.gdu.app08.batch.BoardCountScheduler;
 
 @EnableScheduling  // @Scheduled를 허용한다.
 @Configuration
 public class BatchConfig {
 
 	@Bean
-	public BoardCountAlertScheduler boardCountAlertScheduler() {
-		return new BoardCountAlertScheduler();
+	public BoardCountScheduler boardCountScheduler() {
+		return new BoardCountScheduler();
 	}
 	
 }
