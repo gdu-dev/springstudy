7aa522f8cd33f2465b2818a8ce00fc1dcadc249e
gdu-dev <minkt78@naver.com>
2023-04-24 10:50:28 +0900

    2023-04-24

---
+++ 08_Mybatis_Batch/src/main/java/com/gdu/app08/service/BoardServiceImpl.java
@@ -0,0 +1,82 @@
+package com.gdu.app08.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gdu.app08.domain.BoardDTO;
+import com.gdu.app08.repository.BoardDAO;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	@Override
+	public List<BoardDTO> getBoardList() {
+		return boardDAO.selectBoardList();
+	}
+
+	@Override
+	public BoardDTO getBoardByNo(HttpServletRequest request) {
+		// 파라미터 boardNo가 없으면(null, "") 0을 사용한다.
+		String strBoardNo = request.getParameter("boardNo");
+		int boardNo = 0;
+		if(strBoardNo != null && strBoardNo.isEmpty() == false) {
+			boardNo = Integer.parseInt(strBoardNo);
+		}
+		return boardDAO.selectBoardByNo(boardNo);
+	}
+
+	@Override
+	public int addBoard(HttpServletRequest request) {
+		try {
+			// 파라미터 title, content, writer를 받아온다.
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String writer = request.getParameter("writer");
+			// BoardDAO로 전달할 BoardDTO를 만든다.
+			BoardDTO board = new BoardDTO();
+			board.setTitle(title);
+			board.setContent(content);
+			board.setWriter(writer);
+			return boardDAO.insertBoard(board);
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+	@Override
+	public int modifyBoard(HttpServletRequest request) {
+		try {
+			// 파라미터 title, content, boardNo를 받아온다.
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+			// BoardDAO로 전달할 BoardDTO를 만든다.
+			BoardDTO board = new BoardDTO();
+			board.setTitle(title);
+			board.setContent(content);
+			board.setBoardNo(boardNo);
+			return boardDAO.updateBoard(board);
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+	@Override
+	public int removeBoard(HttpServletRequest request) {
+		try {
+			// 파라미터 boardNo를 받아온다.
+			int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+			return boardDAO.deleteBoard(boardNo);
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+	
+}
