2051418b35ad8c959b6791f4e76eb2588f1ab632
gdu-dev <minkt78@naver.com>
2023-05-08 12:52:53 +0900

    2023-05-08

--- 11_Pjt_Upload/src/main/java/com/gdu/app11/config/FileConfig.java
+++ 11_Pjt_Upload/src/main/java/com/gdu/app11/config/FileConfig.java
@@ -2,18 +2,19 @@ package com.gdu.app11.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 public class FileConfig {
 
 	@Bean
-	public CommonsMultipartResolver commonsMultipartResolver() {
-		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
-		commonsMultipartResolver.setDefaultEncoding("UTF-8");
-		commonsMultipartResolver.setMaxUploadSize(1024 * 1024 * 100);       // 전체 첨부 파일의 크기 100MB
-		commonsMultipartResolver.setMaxUploadSizePerFile(1024 * 1024 * 10); // 첨부 파일 하나의 크기 10MB
-		return commonsMultipartResolver;
+	public MultipartResolver multipartResolver() {  // Bean 이름은 multipartResolver로 설정해야 한다.
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8");
+		multipartResolver.setMaxUploadSize(1024 * 1024 * 100);       // 전체 첨부 파일의 크기 100MB
+		multipartResolver.setMaxUploadSizePerFile(1024 * 1024 * 10); // 첨부 파일 하나의 크기 10MB
+		return multipartResolver;
 	}
 	
 }
