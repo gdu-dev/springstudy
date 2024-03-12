e975b44fe6ba4c4f3d5b416e06aae7b8944943c5
gdu-dev <minkt78@naver.com>
2024-03-12 16:32:07 +0900

    20240312

--- 01_IoC/src/main/java/com/gdu/prj01/xml01/MainClass.java
+++ 01_IoC/src/main/java/com/gdu/prj01/xml01/MainClass.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainClass {
 
-  public static void main(String[] args) {
+  public static void method1() {
     
     // appCtx.xml 읽기
     AbstractApplicationContext ctx = new GenericXmlApplicationContext("com/gdu/prj01/xml01/appCtx.xml");
@@ -21,7 +21,44 @@ public class MainClass {
     
     // appCtx.xml 닫기
     ctx.close();
-
+    
+  }
+  
+  public static void method2() {
+    
+    AbstractApplicationContext ctx = new GenericXmlApplicationContext("com/gdu/prj01/xml01/appCtx.xml");
+    
+    Computer computer1 = ctx.getBean("computer1", Computer.class);
+    
+    System.out.println(computer1.getModel());
+    System.out.println(computer1.getPrice());
+    computer1.getCalculator().add(5, 2);
+    computer1.getCalculator().sub(5, 2);
+    computer1.getCalculator().mul(5, 2);
+    computer1.getCalculator().div(5, 2);
+    
+    ctx.close();
+    
+  }
+  
+  public static void method3() {
+    
+   AbstractApplicationContext ctx = new GenericXmlApplicationContext("com/gdu/prj01/xml01/appCtx.xml");
+   
+   Computer computer2 = ctx.getBean("computer2", Computer.class);
+   System.out.println(computer2.getModel());
+   System.out.println(computer2.getPrice());
+   computer2.getCalculator().add(10, 5);
+   computer2.getCalculator().sub(10, 5);
+   computer2.getCalculator().mul(10, 5);
+   computer2.getCalculator().div(10, 5);
+   
+   ctx.close();
+    
+  }
+  
+  public static void main(String[] args) {
+    method3();
   }
 
 }
