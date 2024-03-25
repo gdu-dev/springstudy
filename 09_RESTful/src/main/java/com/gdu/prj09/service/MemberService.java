ea772ee27d7fc3ce7432837df56d6994ea172725
gdu-dev <minkt78@naver.com>
2024-03-25 11:56:08 +0900

    20240325

--- 09_RESTful/src/main/java/com/gdu/prj09/service/MemberService.java
+++ 09_RESTful/src/main/java/com/gdu/prj09/service/MemberService.java
@@ -2,8 +2,17 @@ package com.gdu.prj09.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.ResponseEntity;
 
+import com.gdu.prj09.dto.MemberDto;
+
 public interface MemberService {
-  public ResponseEntity<Map<String, Object>> getMembers(int page, int display);
+  ResponseEntity<Map<String, Object>> getMembers(int page, int display);
+  ResponseEntity<MemberDto> getMemberByNo(int memberNo);
+  ResponseEntity<Map<String, Object>> registerMember(MemberDto member, HttpServletResponse response);
+  ResponseEntity<Map<String, Object>> modifyMember(MemberDto member);
+  ResponseEntity<Map<String, Object>> removeMember(int memberNo);
+  ResponseEntity<Map<String, Object>> removeMembers(String memberNoList);
 }
---
+++ 09_RESTful/src/main/java/com/gdu/prj09/service/MemberServiceImpl.java
@@ -0,0 +1,58 @@
+package com.gdu.prj09.service;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+
+import com.gdu.prj09.dao.MemberDao;
+import com.gdu.prj09.dto.MemberDto;
+import com.gdu.prj09.utils.MyPageUtils;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+  private final MemberDao memberDao;
+  private final MyPageUtils myPageUtils;
+  
+  @Override
+  public ResponseEntity<Map<String, Object>> getMembers(int page, int display) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<MemberDto> getMemberByNo(int memberNo) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Map<String, Object>> registerMember(MemberDto member, HttpServletResponse response) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Map<String, Object>> modifyMember(MemberDto member) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Map<String, Object>> removeMember(int memberNo) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Map<String, Object>> removeMembers(String memberNoList) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
